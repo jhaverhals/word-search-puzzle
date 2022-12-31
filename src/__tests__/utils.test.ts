8dc4d5d13ee04a656237f63a806c695ce194a2d6
Johan Haverhals <2904623+jhaverhals@users.noreply.github.com>
2022-12-31 22:49:36 +0100

    Fix failing test due to rename

--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -1,19 +1,19 @@
 import {expect} from '@esm-bundle/chai';
-import {toUpperCase} from '../utils';
+import {arrayToUpperCase} from '../utils';
 
-describe('toUpperCase()', function () {
+describe('arrayToUpperCase()', function () {
   it('lower to upper casing', () => {
-    expect(toUpperCase(['a'])).to.deep.equal(['A']);
-    expect(toUpperCase(['a', 'a', 'b'])).to.deep.equal(['A', 'A', 'B']);
-    expect(toUpperCase(['abc', 'def', 'ghi'])).to.deep.equal(['ABC', 'DEF', 'GHI']);
+    expect(arrayToUpperCase(['a'])).to.deep.equal(['A']);
+    expect(arrayToUpperCase(['a', 'a', 'b'])).to.deep.equal(['A', 'A', 'B']);
+    expect(arrayToUpperCase(['abc', 'def', 'ghi'])).to.deep.equal(['ABC', 'DEF', 'GHI']);
   });
   it('mixed lower and upper to upper casing', () => {
-    expect(toUpperCase(['A', 'a', 'B'])).to.deep.equal(['A', 'A', 'B']);
-    expect(toUpperCase(['ABC', 'DeF', 'ghi'])).to.deep.equal(['ABC', 'DEF', 'GHI']);
+    expect(arrayToUpperCase(['A', 'a', 'B'])).to.deep.equal(['A', 'A', 'B']);
+    expect(arrayToUpperCase(['ABC', 'DeF', 'ghi'])).to.deep.equal(['ABC', 'DEF', 'GHI']);
   });
   it('upper remains upper casing', () => {
-    expect(toUpperCase(['A'])).to.deep.equal(['A']);
-    expect(toUpperCase(['A', 'A', 'B'])).to.deep.equal(['A', 'A', 'B']);
-    expect(toUpperCase(['ABC', 'DEF', 'GHI'])).to.deep.equal(['ABC', 'DEF', 'GHI']);
+    expect(arrayToUpperCase(['A'])).to.deep.equal(['A']);
+    expect(arrayToUpperCase(['A', 'A', 'B'])).to.deep.equal(['A', 'A', 'B']);
+    expect(arrayToUpperCase(['ABC', 'DEF', 'GHI'])).to.deep.equal(['ABC', 'DEF', 'GHI']);
   });
 });
